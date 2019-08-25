c450dba84f071b41f8a7450a66d106f9833690e8
Carl Dong <accounts@carldong.me>
2019-08-25 19:47:08 -0400

    Initial commit

---
+++ yo.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+umask "${YO_UMASK:-077}"
+set -o pipefail
+set -e
+
+# Defaults to ~/.config
+TOP_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+# Defaults to ~/.config/yo
+YO_CONFIG_DIR="${YO_CONFIG_DIR:-$TOP_CONFIG_DIR/yo}"
+mkdir -p "$YO_CONFIG_DIR"
+
+# Defaults to ~/.config/token
+YO_TOKEN_PATH="${YO_TOKEN_PATH:-$YO_CONFIG_DIR/token}"
+
+YO_BASE_URL="${YO_BASE_URL:-https://api.yocli.io}"
+
+display_qr() {
+    local title="yo"
+    if [[ -n $DISPLAY || -n $WAYLAND_DISPLAY ]]; then
+        if type feh >/dev/null 2>&1; then
+            echo -n "$1" | qrencode --size 10 -o - | feh -x --title "$title" -g +200+200 - &
+        elif type gm >/dev/null 2>&1; then
+            echo -n "$1" | qrencode --size 10 -o - | gm display -title "$title" -geometry +200+200 - &
+        elif type display >/dev/null 2>&1; then
+            echo -n "$1" | qrencode --size 10 -o - | display -title "$title" -geometry +200+200 - &
+        elif (( 22 < "$(tput lines)")); then
+            echo -n "$1" | qrencode -t utf8
+        else
+            echo "No good way to do this."
+        fi
+    elif (( 22 < "$(tput lines)" )); then
+        echo -n "$1" | qrencode -t utf8
+    else
+        echo "No good way to do this."
+    fi
+}
+
+yo() {
+    curl --silent -o /dev/null -w '%{http_code}' \
+         -X POST -F pc_token="$(< "$YO_TOKEN_PATH")" \
+         -L "${YO_BASE_URL}/yo" 2>/dev/null
+}
+
+yo_repeatedly() {
+    for (( i=0; "$i"<"${3:-256}"; i++ )); do
+        local status
+        status="$(yo)"
+        if (("$status"<"${1}" || "${2}"<="$status" )); then
+            break
+        fi
+        if [ -n "$!" ] && ! ps -p "$!" >/dev/null; then
+            echo "Aborted." >&2
+            exit 1
+        fi
+        sleep 1
+    done
+    echo "$status"
+}
+
+new_token() {
+    local yo_token
+    yo_token="$(uuidgen | tr '[:upper:]' '[:lower:]')"
+    echo "$yo_token" > "$YO_TOKEN_PATH"
+    echo "$yo_token"
+}
+
+link_w_qr() {
+    display_qr "${YO_BASE_URL}/?p=${1}"
+    local status
+    status="$(yo_repeatedly 400 500)"
+    [ -n "$!" ] && kill $!
+    if ((500 <= "$status" && "$status" < 600)); then
+        echo "It seems that the yo backend at '$YO_BASE_URL' is down right now, please try again later."
+    else
+        echo "Sucessfully linked a mobile device!"
+    fi
+}
+
+if [ ! -f "$YO_TOKEN_PATH" ]; then
+    echo "No mobile device linked. Let's fix that :-)"
+    link_w_qr "$(new_token)"
+else
+    status="$(yo_repeatedly 500 600 5)"
+    if [ "$status" -ne 204 ]; then
+        if [ "$status" -eq 404 ]; then
+            echo "No mobile device linked. Let's fix that :-)"
+            link_w_qr "$(new_token)"
+        elif ((500 <= "$status" && "$status" < 600)); then
+            echo "It seems that the yo backend at '$YO_BASE_URL' is down right now, please try again later."
+        else
+            echo "Unknown error."
+        fi
+    fi
+fi
