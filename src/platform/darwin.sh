973fe54c21ad8d6e284f87b0c723e4c46f688144
Carl Dong <contact@carldong.me>
2020-12-20 20:15:25 -0500

    Use spaces instead of tabs

--- src/platform/darwin.sh
+++ src/platform/darwin.sh
@@ -1,6 +1,6 @@
 display_qr() {
-	  if type imgcat >/dev/null 2>&1; then
-		    echo -n "$1" | qrencode --size 10 -o - | imgcat
+    if type imgcat >/dev/null 2>&1; then
+        echo -n "$1" | qrencode --size 10 -o - | imgcat
     else
         text_display_qr "$1"
     fi
