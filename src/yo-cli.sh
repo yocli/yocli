00bece704e6dae36655f4a47ae5454e7b59b4c34
Carl Dong <contact@carldong.me>
2020-12-29 15:28:53 -0500

    Overhaul

--- src/yo-cli.sh
+++ src/yo-cli.sh
@@ -18,6 +18,12 @@ YO_BASE_URL="${YO_BASE_URL:-https://api.yocli.io}"
 
 die() { cat >&2; exit 1; }
 
+bold=$(tput smso)
+nobold=$(tput rmso)
+
+underline=$(tput smul)
+nounderline=$(tput rmul)
+
 server_error_msg() {
 cat <<EOF
 It seems that the yo backend at '$YO_BASE_URL' is down right now, please try
@@ -25,21 +31,94 @@ again later.
 EOF
 }
 
-small_term_msg() {
+term_too_small_msg() {
 cat <<"EOF"
 Your terminal is too small to display the pairing QR Code properly.
 Hint: Try making it larger and invoking `yo` again!
 EOF
 }
 
-new_user_msg() {
+qr_waiting_msg() {
+cat <<EOF
+Waiting for successful pairing...
+
+Hint: Hit Ctrl-C to exit.
+EOF
+}
+
+pair_success_msg() {
+cat <<EOF
+Success: Linked an iOS device!
+
+Hint: Try sending your first notification:
+
+    \$ $PROGRAM
+
+EOF
+}
+
+already_paired_msg() {
+cat <<EOF
+Your computer has already been paired with an iOS device.
+
+Hint: To re-pair your computer with your iOS device, invoke the following:
+
+    \$ $PROGRAM ${underline}re${nounderline}pair
+
+EOF
+}
+
+brand_new_pc_msg() {
 cat <<"EOF"
-It doesn't look like this computer is paired with any iOS devices yet.
-Let's fix that :-)
+Looks like it's your first time using Yo, welcome!
+EOF
+}
 
-A QR code will be displayed below, and you can scan it using the Yo iOS app to
-pair this computer with the iOS device you used to scan.
+qr_scan_prompt_msg() {
+cat <<"EOF"
+Scan the QR code displayed below using the Yo iOS app to pair or re-pair this
+computer with your iOS device.
+EOF
+}
 
+pc_token_not_paired_msg() {
+cat <<EOF
+You must first pair with an iOS device before you can send notifications.
+
+Hint: Invoke the following command to pair with an iOS device:
+
+    \$ $PROGRAM pair
+
+EOF
+}
+
+apns_gone_msg() {
+cat <<EOF
+It would seem that your device has been unregistered from Apple's notification
+services. This can happen if you've uninstalled/reinstalled the Yo app, if
+you've just restored your iOS device from a backup, etc.
+
+Hint: To re-pair your computer with your iOS device, invoke the following:
+
+    \$ $PROGRAM ${underline}re${nounderline}pair
+
+EOF
+}
+
+notification_dispatched_msg() {
+cat <<EOF
+Success: Notification dispatched to your paired iOS device!
+
+Hint: Notifications not showing up? Try re-pairing with:
+
+    \$ $PROGRAM ${underline}re${nounderline}pair
+
+EOF
+}
+
+repair_msg() {
+cat <<EOF
+Removing the existing pairing and establishing a new one!
 EOF
 }
 
@@ -82,57 +161,172 @@ yo() {
 yo_repeatedly() {
     local collapse_id
     collapse_id="$(uuidgen)"
-    log DEBUG <<< "HTTP codes inside ${2}..${3} will prompt a retry up to ${4:-256} times"
-    for (( i=0; "$i"<"${4:-256}"; i++ )); do
+    local max_tries
+    # max_tries="${4:-1800}"
+    max_tries="${4:-10}"
+    log DEBUG <<< "HTTP codes inside ${2}..${3} will prompt a retry up to ${max_tries} times"
+    for (( i=0; "$i"<"$max_tries"; i++ )); do
         local status
         status="$(yo "$1" "$collapse_id" ${5:+"$5"})"
         if (( "$status"<"${2}" || "${3}"<="$status" )); then
+            # Good, exit
             log DEBUG <<< "Try #${i}, status=${status}: OUTside retry range, not retrying anymore"
             break
         else
+            # Bad, retry
             log DEBUG <<< "Try #${i}, status=${status}: INside retry range, retrying in 1 sec..."
             sleep 1
         fi
     done
-    log DEBUG <<< "Final HTTP status: ${status}"
-    echo "$status"
+    if [ "$i" = "$max_tries" ]; then
+        log DEBUG <<< "Timed out after ${max_tries} tries"
+        echo "TIMEOUT"
+    else
+        log DEBUG <<< "Final HTTP status: ${status}"
+        echo "$status"
+    fi
 }
 
 # Usage:
 #     link_w_qr PC_TOKEN
 link_w_qr() {
     local yo_token
-    yo_token="$(uuidgen | tr '[:upper:]' '[:lower:]')"
-    log INFO <<< "Generated PC token: '${yo_token}'"
+    yo_token="$1"
+    qr_scan_prompt_msg
     log INFO <<< "Displaying QR code for '${YO_BASE_URL}/?p=${yo_token}'"
     display_qr "${YO_BASE_URL}/?p=${yo_token}"
+    echo
     log DEBUG <<< "Polling backend for success..."
+    qr_waiting_msg
     local status
     status="$(yo_repeatedly "${yo_token}" 400 500 "" background)"
-    if (( 500 <= "$status" && "$status" < 600 )); then
-        server_error_msg | die
+    if [ "$status" = "TIMEOUT" ]; then
+        die <<< "Pairing timed out."
+    elif (( "$status" < 200 || 300 <= "$status" )); then
+        if (( 500 <= "$status" && "$status" < 600 )); then
+            server_error_msg | die
+        else
+            die <<< "Unknown error. Status code: ${status}"
+        fi
     else
-        echo "Sucessfully linked an iOS device!"
-        log INFO <<< "Saving newly generated PC token to: '$YO_TOKEN_PATH'"
-        echo "$yo_token" > "$YO_TOKEN_PATH"
+        pair_success_msg
     fi
 }
 
 source "$(dirname "$0")/platform/$(uname | cut -d _ -f 1 | tr '[:upper:]' '[:lower:]').sh" 2>/dev/null # PLATFORM_FUNCTION_FILE
 
-if [ ! -e "$YO_TOKEN_PATH" ]; then
-    new_user_msg
-    link_w_qr
-else
-    status="$(yo_repeatedly "$(< "$YO_TOKEN_PATH")" 500 600 5 alert)"
-    if (( "$status" < 200 && 300 <= "$status" )); then
-        if [ "$status" -eq 410 ]; then
-            echo "Mobile device no longer linked. Let's fix that :-)"
-            link_w_qr
+cmd_usage() {
+cat <<EOF
+Usage:
+    $PROGRAM [ping]
+        Send a notification to currently paired mobile devices.
+    $PROGRAM pair
+        Pair with a mobile device for the first time.
+    $PROGRAM repair
+        Change the currently paired mobile device.
+    $PROGRAM help
+        Show this text.
+EOF
+}
+
+is_paired_serverside() {
+    curl --silent -o /dev/null -w '%{http_code}' \
+         -X GET \
+         -F pc_token="$1" \
+         -L "${YO_BASE_URL}/notification_bindings" 2>/dev/null
+}
+
+cmd_pair() {
+    local yo_token
+    yo_token="$(< "$YO_TOKEN_PATH")"
+    local status
+    status="$(is_paired_serverside "$yo_token")"
+    log INFO <<< "notification_bindings: ${status}"
+    if (( "$status" < 200 || 300 <= "$status" )); then
+        # Outside of 2xx
+        if [ "$status" -eq 404 ]; then
+            # pc_token exists, but is unpaired server-side
+            link_w_qr "$yo_token"
         elif (( 500 <= "$status" && "$status" < 600 )); then
             server_error_msg | die
         else
             die <<< "Unknown error. Status code: ${status}"
         fi
+    else
+        # Inside of 2xx
+        already_paired_msg | die
     fi
+}
+
+cmd_repair() {
+    repair_msg
+    echo
+    yo_token="$(uuidgen | tr '[:upper:]' '[:lower:]')"
+    log INFO <<< "Generated PC token: '${yo_token}'"
+    log INFO <<< "Saving newly generated PC token to: '$YO_TOKEN_PATH'"
+    echo "$yo_token" > "$YO_TOKEN_PATH"
+    link_w_qr "$yo_token"
+}
+
+cmd_ping() {
+    local yo_token
+    yo_token="$(< "$YO_TOKEN_PATH")"
+    local status
+    status="$(is_paired_serverside "$yo_token")"
+    log INFO <<< "notification_bindings: ${status}"
+    if (( "$status" < 200 || 300 <= "$status" )); then
+        # Outside of 2xx
+        if [ "$status" -eq 404 ]; then
+            not_initialized_msg | die
+        elif (( 500 <= "$status" && "$status" < 600 )); then
+            server_error_msg | die
+        else
+            die <<< "Unknown error. Status code: ${status}"
+        fi
+    else
+        status="$(yo_repeatedly "$(< "$YO_TOKEN_PATH")" 500 600 5 alert)"
+        if (( "$status" < 200 || 300 <= "$status" )); then
+            if [ "$status" -eq 404 ]; then
+                not_initialized_msg | die
+            elif [ "$status" -eq 410 ]; then
+                apns_gone_msg | die
+            elif (( 500 <= "$status" && "$status" < 600 )); then
+                server_error_msg | die
+            else
+                die <<< "Unknown error. Status code: ${status}"
+            fi
+        else
+            notification_dispatched_msg
+        fi
+    fi
+}
+
+cmd_default() {
+    if [ "$#" -eq 0 ]; then
+        cmd_ping
+    else
+        cmd_usage
+    fi
+}
+
+#
+# END subcommand functions
+#
+
+PROGRAM="${0##*/}"
+
+if [ ! -e "$YO_TOKEN_PATH" ]; then
+    yo_token="$(uuidgen | tr '[:upper:]' '[:lower:]')"
+    log INFO <<< "Generated PC token: '${yo_token}'"
+    brand_new_pc_msg
+    log INFO <<< "Saving newly generated PC token to: '$YO_TOKEN_PATH'"
+    echo "$yo_token" > "$YO_TOKEN_PATH"
 fi
+case "$1" in
+    help|--help) shift;          cmd_usage "$@"   ;;
+    pair) shift;                 cmd_pair "$@"    ;;
+    repair) shift;                 cmd_repair "$@"    ;;
+    ping) shift;                 cmd_ping "$@"    ;;
+    *)                           cmd_default "$@" ;;
+esac
+exit 0
